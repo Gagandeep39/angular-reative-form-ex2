53f9e50c2d3308ee661d05da66f26af2d7c56dcc
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-18 12:00:40 +0530

    Created a dummy Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-reactive-ex2';
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  valueSubscription: Subscription;
+
+  ngOnInit(){
+    this.valueSubscription = this.projectForm.valueChanges.subscribe( (changes) => {
+      this.valueSubscription = changes;
+    });
+  }
+
 }
